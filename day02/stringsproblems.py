714133414f124e1a8eb4436f498a53fc6bc95d45
Ravi Naganuri <ravinaganuri2005@gmail.com>
2026-02-01 14:19:54 +0530

    Update stringsproblems.py

--- day02/stringsproblems.py
+++ day02/stringsproblems.py
@@ -52,3 +52,96 @@ if len(j)==0:
     print("this string is empty ")
 else:
     print("this is not empty")
+
+
+'''Intermediate Level – String Operations & Logic
+Check if a string is a palindrome.
+Count:
+Vowels
+Consonants
+Count number of:
+Words
+Digits
+Spaces
+Replace a word in a string.
+Remove all spaces from a string.
+Find the first and last occurrence of a character.
+Check if two strings are anagrams.'''
+
+#1
+pali = "ravi"
+rev = ""
+for char in pali:
+    rev = char + rev
+    if pali == rev:
+        print ("it is palindrom")
+    else:
+        print("its not palindrom")
+
+s = "madam"
+rev = ""
+
+for ch in s:
+    rev = ch + rev
+
+if s == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+#2
+latter = "programming"
+vowels = 0
+consonants = 0
+for char in latter:
+    if char in "aeiou":
+        vowels += 1
+    else:
+        consonants +=1 
+print("total vowels is ",vowels)
+print("total consonants is ",consonants)
+
+#3
+s = "add31234   "
+word = 0
+spaces = 0
+digits = 0
+for char in s:
+    if char.isalpha():
+        word += 1
+    elif char.isdigit():
+        digits +=1
+    elif char.isspace():
+        spaces += 1
+print("word is ",word)
+print("digits is ",digits)
+print("spaces is ",spaces)
+
+#4
+p = "I love Python"
+old_word = "Python"
+new_word = "Java"
+
+result = ""
+world = ""
+
+for ch in p + " ":          # extra space to process last word
+    if ch != " ":
+        world += ch
+    else:
+        if world == old_word:
+            result += new_word + " "
+        else:
+            result += world + " "
+        world = ""
+print(result.strip())
+
+#5
+m = " P ra  jwal  "
+result =""
+for char in m:
+    if char !=" ":
+        result += char
+print(result)
+
