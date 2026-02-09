87429657e1c7ccce772cda4d72712314c5812f36
Ravi Naganuri <ravinaganuri2005@gmail.com>
2026-02-09 08:18:57 +0530

    Update loops.py

--- day04/loops.py
+++ day04/loops.py
@@ -7,3 +7,17 @@ print("Reversed string:", reverse)
 text = "hello"
 reversed_text = ''.join(reversed(text))
 print(reversed_text)  # Output: "olleh"
+
+squares=[number ** 2 for number in range(6)]
+print(squares)
+
+names=["Ravi","vinod","abhi","prashant"]
+for name in names:
+    print(name)
+
+# breaking the loop
+for index in range(10):
+    if index == 5:
+        print("breaking the loop")
+        break
+    print(index)
