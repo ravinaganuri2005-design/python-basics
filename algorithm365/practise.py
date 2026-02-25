c3026929ea2c6d73b1307e18ecbfc67a53054354
Ravi Naganuri <ravinaganuri2005@gmail.com>
2026-02-25 16:29:05 +0530

    Implement edge star printing function
    Added a function to print edge stars for a grid.

--- algorithm365/practise.py
+++ algorithm365/practise.py
@@ -13,3 +13,16 @@ numbers=[10,20,30,40,50,60]
 key=int(input("enter a number you want: "))
 #calling or invoke
 len_search(numbers,key)
+
+
+# print the edges stars
+def print_edge_stars(gridsize):
+    for  row in range(1, gridsize+1 ,1):
+        for column in range(1, gridsize +1, 1):
+            if row == 1 or row == gridsize or column == 1 or column == gridsize:
+                print("*" ,end=" ")
+            else:
+                print(" ",end=" ")
+        print()    
+print_edge_stars(5) 
+
