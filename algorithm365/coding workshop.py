9eaa5e7ec2abd26286e213637b3555b0d8df9275
Ravi Naganuri <ravinaganuri2005@gmail.com>
2026-03-05 21:21:55 +0530

    Create coding workshop.py

---
+++ algorithm365/coding workshop.py
@@ -0,0 +1,14 @@
+#function to get sum of two numbers
+
+# declare the function
+def get_sum(number1,number2):
+    return(number1 + number2 )
+
+#invocation or calling the function
+sum = get_sum(20,30)
+print("the sum of two numbers",sum)
+
+sum2 = get_sum(1.3 , 2.5)
+print("the sum of two float numbers",sum2)
+
+
