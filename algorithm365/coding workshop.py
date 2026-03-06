d6c1d2c2a681a1d97b0af8d8a04a0e24b3c1931e
Ravi Naganuri <ravinaganuri2005@gmail.com>
2026-03-06 15:14:02 +0530

    Update coding workshop.py

--- algorithm365/coding workshop.py
+++ algorithm365/coding workshop.py
@@ -11,4 +11,26 @@ print("the sum of two numbers",sum)
 sum2 = get_sum(1.3 , 2.5)
 print("the sum of two float numbers",sum2)
 
+#swap the numbers between two numbers
+
+def swap(item1, item2):
+    temp = item1
+    item1 = item2
+    item2 = temp
+    return item1, item2
+
+# def swapSimple(item1, item2):    
+#     return item2, item1
+
+def invoke_swap():
+    number1 = 10
+    number2 = 20
+
+    print(f"Before swapping: Value of number1 = {number1} and number2 = {number2}")
+    number1, number2 = swap(number1, number2)
+
+    # number1, number2 = swapSimple(number1, number2)
+    print(f"After swaping: Value of number1 = {number1} and number2 = {number2}")
+invoke_swap()
+
 
