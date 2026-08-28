cdd3b80c69b88955eab3a90d35794bbba0757ed7
Ravi Naganuri <ravinaganuri2005@gmail.com>
2026-08-28 15:48:41 +0530

    Update practise.py

--- algorithm365/practise.py
+++ algorithm365/practise.py
@@ -92,3 +92,20 @@ if total == original:
 else:
     print("It is not armstrong number")
 
+#Reverse Number
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = 1
+        if x < 0:
+            sign = -1
+            x = abs(x)
+        reverse_number = 0
+        while x > 0:
+            lastdigit = x % 10
+            reverse_number = reverse_number * 10 + lastdigit
+            x = x // 10
+        reverse_number = reverse_number * sign
+        if reverse_number < -2**31 or reverse_number > 2**31 - 1:
+            return 0
+        return reverse_number
+
